5350565f5a194187f427b85ebbb1a59636344212
Niklas Andersson <niklas.andersson@openforce.se>
2019-03-26 03:38:22 +0100

    Fixed bugs

--- src/install.py
+++ src/install.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from jinja2 import Template
 import os
 import subprocess
@@ -17,33 +18,44 @@ def install_slapd(domain=None, password=None, organization=None):
     proc.wait()
     proc = subprocess.Popen(['apt-get', 'install', 'slapd', 'ldap-utils', '-y'], stderr=open(os.devnull, 'w'))
     proc.wait()
-    proc = subprocess.Popen(['slapadd', '-v', '-F', '/etc/ldap/slapd.d', '-l', '/tmp/sudo.ldif', '-b', "'cn=config'"])
+    proc = subprocess.Popen(['slapadd', '-v', '-F', '/etc/ldap/slapd.d/', '-l', '/tmp/sudo.ldif', '-b', 'cn=config'])
     proc.wait()
     t = Template(open('/tmp/domain.ldif.jinja2').read())
     with open('/tmp/domain.ldif', 'w') as f:
         f.write(t.render(
-            dc = domain.split('.')[0]
+            dc = domain.split('.')[0],
             tld = domain.split('.')[1]
         ))
     proc = subprocess.Popen(['slapadd', '-l', '/tmp/domain.ldif'])
     proc.wait()
+    t = Template(open('/tmp/nandersson.ldif.jinja2').read())
+    with open('/tmp/nandersson.ldif', 'w') as f:
+        f.write(t.render(
+                    dc = domain.split('.')[0],
+                    tld = domain.split('.')[1]
+        ))
+    proc = subprocess.Popen(['slapadd', '-l', '/tmp/nandersson.ldif'])
+    proc.wait()
     proc = subprocess.Popen(['chown', '-R', 'openldap:openldap', '/var/lib/ldap'])
     proc.wait()
-    proc = subprocess.Popen(['chmod', '0600', '/var/lib/ldap/*'])
+    proc = subprocess.Popen(['chmod', '-R', '0600', '/var/lib/ldap/'])
     proc.wait()
-    proc = subprocess.Popen(['chown', '-R', 'openldap:openldap', '/etc/ldap/slapd.d/*'])
+    proc = subprocess.Popen(['chmod', '0700', '/var/lib/ldap'])
+    proc.wait()
+    proc = subprocess.Popen(['chown', '-R', 'openldap:openldap', '/etc/ldap/slapd.d/'])
     proc.wait()
     t = Template(open('/tmp/ldap.conf.jinja2').read())
     with open('/etc/ldap/ldap.conf', 'w') as f:
         f.write(t.render(
-            dc = domain.split('.')[0]
+            dc = domain.split('.')[0],
             tld = domain.split('.')[1]
         ))
 
 def slapd():
-    proc = subprocess.Popen(['/usr/sbin/slapd', '-d', '5', '-h', '"ldap:/// ldapi:///"', '-g', 'openldap', '-u', 'openldap', '-F', '/etc/ldap/slapd.d'])
+#proc = subprocess.Popen(["/usr/sbin/slapd", "-d", "5", "-h", "'ldap:/// ldapi:///'", "-g", "openldap", "-u", "openldap", "-F", "/etc/ldap/slapd.d"])
+    proc = subprocess.Popen(['/usr/sbin/slapd', '-d', '5', '-h', 'ldap:/// ldapi:///', '-g', 'openldap', '-u', 'openldap', '-F', '/etc/ldap/slapd.d'])
     proc.wait()
 
 if __name__ == '__main__':
     install_slapd(domain=DOMAIN, password=PASSWORD, organization=ORGANIZATION)
-    #slapd()
+    slapd()
